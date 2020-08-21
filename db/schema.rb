fd66b05b120dda7b9aa552caf576d508f0879a96
t-yoshida0721 <68986740+t-yoshida0721@users.noreply.github.com>
2020-08-21 17:19:31 +0900

    itemモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_103127) do
+ActiveRecord::Schema.define(version: 2020_08_21_074314) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "picture", null: false
+    t.string "product_name", null: false
+    t.text "product_description", null: false
+    t.integer "product_category", null: false
+    t.integer "Product_status", null: false
+    t.integer "burden", null: false
+    t.integer "area", null: false
+    t.integer "days", null: false
+    t.integer "selling_prise", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
