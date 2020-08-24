58973ec5ab67b911b172c3746a0016a95b9fe0fb
t-yoshida0721 <68986740+t-yoshida0721@users.noreply.github.com>
2020-08-24 20:30:54 +0900

    ログインからトップページに推移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
   end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [])
+  end
 end
