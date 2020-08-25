95bbf9d65e686125bac7dc74799087e84aa71919
t-yoshida0721 <68986740+t-yoshida0721@users.noreply.github.com>
2020-08-25 23:24:43 +0900

    エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birthday, presence: true
   validates :email, presence: true  
   validates :password, presence: true  
   validates :password_confirmation, presence: true
