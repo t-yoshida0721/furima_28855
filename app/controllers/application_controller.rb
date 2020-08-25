ca15fd8a1895ae7532a801bcc2705e2c733c994c
t-yoshida0721 <68986740+t-yoshida0721@users.noreply.github.com>
2020-08-25 16:06:08 +0900

    バリデーションの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
   end
 
-  private
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in, keys: [])
-  end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+  validates :nickname, presence: true
+  validates :email, presence: true  
+  validates :password, presence: true  
+  validates :password_confirmation, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6 }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+
 end
