37cfadaea05cf54f4ab75c4ba795e637d5b62261
t-yoshida0721 <68986740+t-yoshida0721@users.noreply.github.com>
2020-08-27 20:56:27 +0900

    商品出品ページに推移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,33 @@ class ItemsController < ApplicationController
   def index  
   end
 
-  
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(tweet_params)
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:picture, :product_name, :product_description, :product_category, :product_status, :burden, :area, :days, :selling_prise, :user)
+  end
+
 
 end
