92c53fe7448320d60f245463c354c92b4d37125d
t-yoshida0721 <68986740+t-yoshida0721@users.noreply.github.com>
2020-09-01 22:13:13 +0900

    itemモデル単体テストコード

---
+++ spec/factories/items.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :item do
+    user
+    product_name            {"abe"}
+    product_description     {"あああ"}
+    product_category_id     {"1"}
+    Product_status_id       {"1"}
+    burden_id               {"1"} 
+    area_id                 {"1"}
+    days_id                 {"1"}
+    selling_prise           {"300"}
+  end
+end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,81 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  describe '#create' do
+    before do
+      @item = FactoryBot.build(:item)
+      @item.image = fixture_file_upload('public/images/test_image.png')
+end
+describe '商品出品' do
+ context '商品出品がうまくいく時' do
+
+    it "全ての項目が存在すれば出品できる" do
+      expect(@item).to be_valid
+    end
+  end
+
+  context '商品出品がうまくいかないとき' do
+
+    it  "product_nameが空だと登録できない" do
+      @item.product_name = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Product name can't be blank")
+    end
+
+    it  " product_descriptionが空だと登録できない" do
+      @item.product_description  = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Product description can't be blank")
+    end
+
+    it  "product_category_idが空だと登録できない" do
+      @item.product_category_id = "0"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Product category is not included in the list")
+    end
+
+    it  "Product_status_idが空だと登録できない" do
+      @item.Product_status_id = "0"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Product status is not included in the list")
+    end
+
+    it  "burden_idが空だと登録できない" do
+      @item.burden_id  = "0"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Burden is not included in the list")
+    end
+
+    it  "area_idが空だと登録できない" do
+      @item.area_id = "0"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Area is not included in the list")
+    end
+
+    it  "days_idが空だと登録できない" do
+      @item.days_id = "0"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Days is not included in the list")
+    end
+
+    it  "selling_priseが空だと登録できない" do
+      @item.selling_prise = "0"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Selling prise is not included in the list")
+    end
+
+    it  "selling_priseが半角数字でなければ登録できない" do
+      @item.selling_prise = "５５５５"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Selling prise is not included in the list")
+    end
+    
+    it  "selling_priseが300~9999999の間でなければ登録できない" do
+      @item.selling_prise = "299"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Selling prise is not included in the list")
+    end
+  end
+end
+end
+end
