dbc0bae16d2b07c6ac657b1798abb98ed7fc26fc
t-yoshida0721 <68986740+t-yoshida0721@users.noreply.github.com>
2020-09-03 16:16:00 +0900

    未ログインユーザーが詳細ページに推移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
 
   before_action :set_item, only: [:edit, :show, :update, :destroy]
-  before_action :authenticate_user! , except: [:index]
-
+  before_action :authenticate_user! , except: [:index,:show]
+  
   def index  
     @items = Item.all
   end
